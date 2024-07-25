77904cce98636f48a7e8c0e246215bb0553fc9f0
Daniel Morales <danny.mora542@gmail.com>
2024-07-24 20:14:13 -0700

    Added a function for checking winner

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function getComputerChoice () {
 
-    let validChoices = ['rock', 'paper', 'scissors']
-
     let rand = Math.floor(Math.random() * 3)
     rand = validChoices[rand]
     return rand
@@ -9,7 +7,6 @@ function getComputerChoice () {
 
 function getUserChoice() {
 
-    let validChoices = ['rock', 'paper', 'scissors']
     let userChoice 
     let isInvalid = true
     do {
@@ -20,15 +17,47 @@ function getUserChoice() {
             userChoice == validChoices[2]) 
                    isInvalid = false
     } while(isInvalid)
-    //userChoice = userChoice.toLowerCase()
+
     return userChoice.toLowerCase()
 
 }
 
+function winChecker(playerChoice, computerChoice) {
+
+    let winner
+
+    if (playerChoice == computerChoice) {
+        winner = 'tie'
+    }
+
+    else if ((playerChoice == validChoices[0] & computerChoice == validChoices[2]) | 
+        (playerChoice == validChoices[1] & computerChoice == validChoices[0]) |
+        (playerChoice == validChoices[2] & computerChoice == validChoices[1])) {
+            winner = 'player'
+        }
+    else {
+        winner = 'computer'
+    }
+
+        return winner
+
+
+}
+
+function playRound() {
+
+   // let validChoices = ['rock', 'paper', 'scissors']
+    let playerChoice = getUserChoice()
+    let computerChoice = getComputerChoice()
+
+    console.log (playerChoice)
+    console.log (computerChoice)
+
+    console.log (winChecker(playerChoice, computerChoice))
+}
+
+let validChoices = ['rock', 'paper', 'scissors']
+
+
+playRound()
 let humanScore = 0, computerScore = 0
-
-let playerChoice = getUserChoice()
-console.log(playerChoice)
-
-let computerChoice = getComputerChoice()
-console.log(computerChoice)
