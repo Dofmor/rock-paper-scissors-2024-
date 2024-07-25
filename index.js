16b547cc01d63a7e38b9fc4558823b0d3ef010dc
Daniel Morales <danny.mora542@gmail.com>
2024-07-24 21:01:56 -0700

    Added logic for game starting and ending

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
 
     let rand = Math.floor(Math.random() * 3)
     rand = validChoices[rand]
@@ -7,16 +7,16 @@ function getComputerChoice () {
 
 function getUserChoice() {
 
-    let userChoice 
+    let userChoice
     let isInvalid = true
     do {
 
         userChoice = prompt("Rock, Paper, or Scissors?")
 
         if (userChoice == validChoices[0] | userChoice == validChoices[1] |
-            userChoice == validChoices[2]) 
-                   isInvalid = false
-    } while(isInvalid)
+            userChoice == validChoices[2])
+            isInvalid = false
+    } while (isInvalid)
 
     return userChoice.toLowerCase()
 
@@ -30,34 +30,58 @@ function winChecker(playerChoice, computerChoice) {
         winner = 'tie'
     }
 
-    else if ((playerChoice == validChoices[0] & computerChoice == validChoices[2]) | 
+    else if ((playerChoice == validChoices[0] & computerChoice == validChoices[2]) |
         (playerChoice == validChoices[1] & computerChoice == validChoices[0]) |
         (playerChoice == validChoices[2] & computerChoice == validChoices[1])) {
-            winner = 'player'
-        }
+        winner = 'player'
+    }
     else {
         winner = 'computer'
     }
 
-        return winner
+    return winner
 
 
 }
 
-function playRound() {
 
-   // let validChoices = ['rock', 'paper', 'scissors']
-    let playerChoice = getUserChoice()
-    let computerChoice = getComputerChoice()
+function playGame() {
 
-    console.log (playerChoice)
-    console.log (computerChoice)
+    let playerPoints = 0, computerPoints = 0
+    const MAX_GAMES = 5
 
-    console.log (winChecker(playerChoice, computerChoice))
+    function playRound() {
+        // let validChoices = ['rock', 'paper', 'scissors']
+        let playerChoice = getUserChoice()
+        let computerChoice = getComputerChoice()
+
+        console.log(playerChoice)
+        console.log(computerChoice)
+
+        console.log(winChecker(playerChoice, computerChoice))
+
+        let winner = winChecker(playerChoice, computerChoice)
+        console.log(winner)
+
+        if (winner == 'player') {
+            playerPoints += 1
+        }
+        else if (winner == 'computer') {
+            computerPoints += 1
+        }
+
+        console.log(`player points: ${playerPoints}`)
+        console.log(`computer points: ${computerPoints}`)
+
+    }
+
+    while ((playerPoints < MAX_GAMES) & (computerPoints < MAX_GAMES)) {
+        playRound()
+    }
+
 }
 
 let validChoices = ['rock', 'paper', 'scissors']
+playGame()
 
 
-playRound()
-let humanScore = 0, computerScore = 0
